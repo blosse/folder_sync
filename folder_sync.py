9ecaf8fa9ba11c575c8fbce459ad6a00cbf834b5
Titus Blosse <titus@student.chalmers.se>
2024-04-05 15:05:21 +0200

    Walking the directories seems to work kinda.

--- folder_sync.py
+++ folder_sync.py
@@ -15,6 +15,14 @@ import os
 import shutil
 import argparse
 
+def check_files(src_dir, dst_dir, files) :
+        # Check if file exists in dst_dir
+        for file in files :
+            src_file = os.path.join(src_dir, file)
+            dst_file = os.path.join(dst_dir, file)
+            if not os.path.exists(dst_file) or os.stat(src_file).st_mtime > os.stat(src_file).st_mtime :
+                print("Something needs to be synced!")
+
 def sync_folders(src_folder, dst_folder) :
     #Check that both folders exist
     if not os.path.isdir(src_folder) :
@@ -25,17 +33,25 @@ def sync_folders(src_folder, dst_folder) :
     #Walk src dir
     for src_dir, dir_names, files in os.walk(src_folder) :
         #Check if dirs exist in dst_folder
-        dst_dir = dir_names.replace(src_folder, dst_folder, 1)
+        for dir in dir_names :
+            dst_dir = os.path.join(dst_folder, dir)
+            print(f"Checking for dir: '{dst_dir}'")
 
-        if not os.path.isdir(dst_dir) :
-            os.makedirs(dst_dir)
+            if not os.path.isdir(dst_dir) :
+                os.makedirs(dst_dir)
+                print(f"Dir not found, created new dir: {dst_dir}")
 
-        # Check if file exists in dst_dir
         for file in files :
             src_file = os.path.join(src_dir, file)
-            dst_file = os.path.join(dst_dir, file)
+            dst_file = src_file.replace(src_folder, dst_folder, 1)
+            print(f"Checking for file '{src_file}'")
+
             if not os.path.exists(dst_file) or os.stat(src_file).st_mtime > os.stat(src_file).st_mtime :
-                print("Something needs to be synced!")
+                print(f"File: '{dst_file}' not found")
+                file_path = shutil.copy2(src_file, dst_file)
+                print(f"Created file: '{file_path}'")
+
+            #check_files(src_dir, dst_dir, files)
            
 # Run the thing
 def main() :
@@ -44,9 +60,11 @@ def main() :
     parser = argparse.ArgumentParser(description='Synchronize folders.')
     parser.add_argument('src', type=str, help='Source folder to sync from')
     parser.add_argument('dst', type=str, help='Destination folder to sync to') 
-    parser.add_argument('interval', type=integer, help='Interval between syncs in seconds')
+    parser.add_argument('interval', type=int, help='Interval between syncs in seconds')
 
-    args = parser.parse_arg()
+    args = parser.parse_args()
+
+    sync_folders(args.src, args.dst)
 
 if __name__ == "__main__" :
     main()
